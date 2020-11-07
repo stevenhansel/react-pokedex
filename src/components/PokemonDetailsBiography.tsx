27fc8764c86132505a98f1a993a4c288d9a66e06
ShinteiMai <stevenhanselgo@gmail.com>
2020-11-07 21:13:08 +0700

    :recycle: Finished refactoring PokemonDetails Biography

--- src/components/PokemonDetailsBiography.tsx
+++ src/components/PokemonDetailsBiography.tsx
@@ -3,6 +3,7 @@ import { Pokemon } from "../features/pokemonSlice";
 import { Species } from "../features/speciesSlice";
 import { importImages } from "../globals";
 import { leftPad } from "../utils/leftPad";
+import PokemonInformation from "./PokemonInformation";
 
 type Props = {
   pokemon: Pokemon;
@@ -27,84 +28,84 @@ const PokemonDetailsBiography = ({ pokemon, species }: Props) => {
           }
         </p>
         <ul className="mt-5">
-          <li className="grid grid-cols-2 gap-x-1 mb-3">
-            <span className="opacity-75">Species</span>
-            <span>
-              {species.genera.find((gen) => gen.language.name === "en")?.genus}
-            </span>
-          </li>
-          <li className="grid grid-cols-2 gap-x-1 mb-3">
-            <span className="opacity-75">Height</span>
-            <span>
-              {feet}'{leftPad(Number(inches) % 12, 2)}" ({pokemon.height / 10}m)
-            </span>
-          </li>
-          <li className="grid grid-cols-2 gap-x-1 mb-3">
-            <span className="opacity-75">Weight</span>
-            <span>{(pokemon.weight / 10).toFixed(1)} kg</span>
-          </li>
-          <li className="grid grid-cols-2 gap-x-1 mb-3">
-            <span className="opacity-75">Abilities</span>
-            <ol>
-              {pokemon.abilities.map((ability, index) => (
-                <li
-                  key={`ability=${ability.ability.name}`}
-                  className="capitalize"
-                >
-                  {index + 1}. {ability.ability.name}{" "}
-                  {ability.isHidden && "(Hidden Ability)"}
-                </li>
-              ))}
-            </ol>
-          </li>
-          <li className="grid grid-cols-2 gap-x-2 mb-3">
-            <span className="opacity-75">Gender</span>
-            <span className="flex items-end justify-start">
-              {genderPercentage === -1 ? (
-                <span>Genderless</span>
-              ) : (
-                <>
-                  <div className="flex items-center mr-3">
-                    <img
-                      className="w-4 h-4"
-                      src={importImages("male")}
-                      alt="male"
-                    />
-                    <span className="ml-2">{100 - genderPercentage}%</span>
-                  </div>
-                  <div className="flex items-center">
-                    <img
-                      className="w-4 h-4"
-                      src={importImages("female")}
-                      alt="female"
-                    />
-                    <span className="ml-2">{genderPercentage}%</span>
-                  </div>
-                </>
-              )}
-            </span>
-          </li>
+          <PokemonInformation
+            title="Species"
+            content={
+              species.genera.find((gen) => gen.language.name === "en")?.genus
+            }
+          />
+          <PokemonInformation
+            title="Height"
+            content={`${feet}'${leftPad(Number(inches) % 12, 2)}" (${
+              pokemon.height / 10
+            }m)`}
+          />
+          <PokemonInformation
+            title="Weight"
+            content={`${(pokemon.weight / 10).toFixed(1)} kg`}
+          />
+          <PokemonInformation
+            title="Abilities"
+            content={pokemon.abilities.map((ability, index) => (
+              <li
+                key={`ability=${ability.ability.name}`}
+                className="capitalize"
+              >
+                {index + 1}. {ability.ability.name}{" "}
+                {ability.isHidden && "(Hidden Ability)"}
+              </li>
+            ))}
+          />
+          <PokemonInformation
+            title="Gender"
+            content={
+              <span className="flex items-end justify-start">
+                {genderPercentage === -1 ? (
+                  <span>Genderless</span>
+                ) : (
+                  <>
+                    <div className="flex items-center mr-3">
+                      <img
+                        className="w-4 h-4"
+                        src={importImages("male")}
+                        alt="male"
+                      />
+                      <span className="ml-2">{100 - genderPercentage}%</span>
+                    </div>
+                    <div className="flex items-center">
+                      <img
+                        className="w-4 h-4"
+                        src={importImages("female")}
+                        alt="female"
+                      />
+                      <span className="ml-2">{genderPercentage}%</span>
+                    </div>
+                  </>
+                )}
+              </span>
+            }
+          />
         </ul>
       </div>
       <div className="my-8">
         <h2 className="font-semibold text-lg">Training</h2>
         <ul className="mt-5">
-          <li className="grid grid-cols-2 gap-x-1 mb-3">
-            <span className="opacity-75">Base Exp</span>
-            <span>{pokemon.baseExperience || 0}</span>
-          </li>
-          <li className="grid grid-cols-2 gap-x-1 mb-3">
-            <span className="opacity-75">Base Happiness</span>
-            <span>{species.baseHappiness || 0}</span>
-          </li>
-          <li className="grid grid-cols-2 gap-x-1 mb-3">
-            <span className="opacity-75">Catch Rate</span>
-            <span>{((species.captureRate / 255) * 100).toFixed(1)}%</span>
-          </li>
-          <li className="grid grid-cols-2 gap-x-1 mb-3">
-            <span className="opacity-75">Growth Rate</span>
-            <span className="capitalize">{species.growthRate.name}</span>
-          </li>
+          <PokemonInformation
+            title="Base Exp"
+            content={pokemon.baseExperience || 0}
+          />
+          <PokemonInformation
+            title="Base Happiness"
+            content={species.baseHappiness || 0}
+          />
+          <PokemonInformation
+            title="Catch Rate"
+            content={`${((species.captureRate / 255) * 100).toFixed(1)}%`}
+          />
+          <PokemonInformation
+            title="Growth Rate"
+            content={species.growthRate.name}
+          />
         </ul>
       </div>
     </>
