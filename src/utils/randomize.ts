3a344e9d3bdf9445947851dc28a0858c46f46c86
Juan Vasquez <juan@logicalbricks.com>
2020-11-15 00:15:32 -0600

    add randomize testing

--- src/utils/randomize.ts
+++ src/utils/randomize.ts
@@ -1,2 +1,2 @@
-export const randomize = (minimum: number, maximum: number) =>
+export const randomize = (minimum: number, maximum: number): number =>
   Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
